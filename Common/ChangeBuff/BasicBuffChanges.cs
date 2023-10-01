09955fdb7e553dd3d0012e29dd9c61cf1a343ef0
Karma <67715746+Karma428507@users.noreply.github.com>
2023-09-30 23:12:22 -0400

    Added from the other branch, sorry for the inconvenince

---
+++ Common/ChangeBuff/BasicBuffChanges.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Alteria.Common.ChangeBuff
+{
+    /// <summary>
+    /// This is just for simple value changes in buffs
+    /// </summary>
+    public class BasicBuffChanges : GlobalBuff
+    {
+        public override void Update(int type, Player player, ref int buffIndex)
+        {
+
+            switch (type)
+            {
+                case BuffID.Ichor:
+                    player.statDefense -= 5;
+                    break;
+                case BuffID.Venom:
+                    player.statDefense -= 10;
+                    break;
+            }
+        }
+
+        // This is just like player except for npcs (if you couldn't tell already)
+        public override void Update(int type, NPC npc, ref int buffIndex)
+        {
+            switch (type)
+            {
+                case BuffID.Ichor:
+                    npc.defense -= 5;
+                    break;
+                case BuffID.Venom:
+                    npc.defense -= 10;
+                    break;
+            }
+        }
+    }
+
+    /*
+    IGNORE
+
+    public class ILBuffChanges // DON'T MAKE IT AND INFERFACE YOU DAM IDE
+    {
+        public static void IL_Player_UpdateBuffs(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            if (!c.TryGotoNext(
+                i => i.MatchLdarg(0),
+                i => i.MatchLdfld(typeof(Player), nameof(Player.venom))
+                )) return;
+            c.Index += 14;
+            c.Remove();
+            c.EmitLdcI4(-15);
+        }
+        public static void IL_NPC_UpdateBuffs(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            if (!c.TryGotoNext(
+                i => i.MatchLdarg(0),
+                i => i.MatchLdfld(typeof(NPC), nameof(NPC.venom))
+                )) return;
+            c.Index += 13;
+            c.Remove();
+            c.EmitLdcI4(-30);
+        }
+    }
+     
+     */
+}
---
+++ Common/changetile/TempleChange.cs
@@ -0,0 +1,47 @@
+﻿using MonoMod.Cil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace Alteria.Common.ChangeTile
+{
+    public class EarlyTemple : ModPlayer
+    {
+        public override void Load()
+        {
+            IL_Player.ItemCheck_UseTeleportRod += IL_Remove_Wall_Condition;
+            IL_Player.CanDoWireStuffHere += IL_Remove_Wall_Condition;
+            IL_Wiring.HitWireSingle += IL_Remove_Wall_Condition;
+
+            IL_Wiring.DeActive += IL_Remove_Brick_Condition;
+        }
+
+        private void IL_Remove_Wall_Condition(ILContext il)
+        {
+            var c = new ILCursor(il);
+
+            if (!c.TryGotoNext(
+                i => i.MatchLdcI4(87)
+                )) return;
+
+            c.Remove();
+            c.EmitLdcI4(-1);
+        }
+
+        private void IL_Remove_Brick_Condition(ILContext il)
+        {
+            var c = new ILCursor(il);
+
+            if (!c.TryGotoNext(
+                i => i.MatchLdcI4(226)
+                )) return;
+
+            c.Remove();
+            c.EmitLdcI4(-1);
+        }
+    }
+}
