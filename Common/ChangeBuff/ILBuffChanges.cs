086dcb43834ef787dcab83647ad91fb2df2ed159
Karma <67715746+Karma428507@users.noreply.github.com>
2023-09-30 22:42:56 -0400

    Added my work

---
+++ Common/ChangeBuff/ILBuffChanges.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace CombatPlus.Common.ChangeBuff
+{
+    /// <summary>
+    /// This is where the methods for IL changes are
+    /// </summary>
+    /*public class ILBuffChanges // DON'T MAKE IT AND INFERFACE YOU DAM IDE
+    {
+        public static void IL_Player_UpdateBuffs(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            if (!c.TryGotoNext(
+                i => i.MatchLdarg(0),
+                i => i.MatchLdfld(typeof(Player), nameof(Player.venom))
+                )) return;
+
+            c.Index += 14;
+            c.Remove();
+            c.EmitLdcI4(-15);
+        }
+
+        public static void IL_NPC_UpdateBuffs(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            if (!c.TryGotoNext(
+                i => i.MatchLdarg(0),
+                i => i.MatchLdfld(typeof(NPC), nameof(NPC.venom))
+                )) return;
+
+            c.Index += 13;
+            c.Remove();
+            c.EmitLdcI4(-30);
+        }
+    } fix later */
+}
---
+++ Common/ChangeItem/ShimmerFix.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria;
+using Terraria.ModLoader;
+using MonoMod.Cil;
+
+namespace CombatPlus.Common.ChangeItem
+{
+    public class ShimmerFix : GlobalItem
+    {
+        private List<int> CannotShimmer = new List<int>()
+        {
+            // Put whatever items you don't want here
+        };
+
+        public override void Load()
+        {
+            Terraria.On_Item.CanShimmer += On_Item_CanShimmer;
+        }
+
+        private bool On_Item_CanShimmer(On_Item.orig_CanShimmer orig, Item self)
+        {
+            foreach (int Item in CannotShimmer)
+                if (self.type == Item)
+                    return false;
+
+            orig.Invoke(self);
+            return true; // The award for the most useless piece of code goes to...
+        }
+    }
+}
